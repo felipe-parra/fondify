8d86b87542aab994b00f23709e2edfeae74adef5
pipepico <fpr29.10@gmail.com>
2019-04-29 10:46:55 -0500

    models

--- models/Dish.js
+++ models/Dish.js
@@ -1,2 +1,12 @@
 const mongoose    = require('mongoose')
 const { Schema }  = mongoose
+
+const dishSchema = new Schema({
+  name: String,
+  meal: {
+    type: String,
+    enum: ['Entrada', 'Plato Fuerte', 'Postre','Bebida']
+  }
+})
+
+module.exports = mongoose.model('Dish', dishSchema)
--- models/Menu.js
+++ models/Menu.js
@@ -2,6 +2,11 @@ const mongoose    = require('mongoose')
 const { Schema }  = mongoose
 
 const menuSchema  = new Schema ({
+  name: String,
+  entradas: [String],
+  platoFuertes: [String],
+  postres: [String],
+  bebidas: [String]
 
 })
 
