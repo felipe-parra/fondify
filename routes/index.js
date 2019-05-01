0c98317fcb8f8b4d75062cb21e05323802e5b59b
pipepico <fpr29.10@gmail.com>
2019-05-01 15:31:41 -0500

    make route & start view to dashboard fonda

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const express       = require('express');
-const { isLogged }  = require('../helpers/middlewares')
 const router        = express.Router();
+const Menu = require('../models/Menu')
+const Fonda = require('../models/Fonda')
+const Order = require('../models/Order')
+const MenuUser = require('../models/MenuUser')
+const { isLogged, checkRole } = require('../helpers/middlewares')
+const moment = require('moment')
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/fonda', isLogged , checkRole('fonda'),(req,res,next) => {
+  Fonda.find({user: req.user._id})
+    .then(fonda => {
+      Order.find({fonda: fonda._id}).populate('user').populate('menuUser')
+        .then(order => {
+          res.render('fondas/dashboard', {order,userName: req.user})
+        })
+        .catch(err => next(err))
+    })
+    .catch(err => next(err))
+})
 
 module.exports = router;
